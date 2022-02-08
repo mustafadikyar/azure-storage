f703faac8364850078fefda1aa91d925bf3795d5
Mustafa Dikyar <40761919+mustafadikyar@users.noreply.github.com>
2022-02-08 21:55:46 +0300

    Azure Queue Storage

---
+++ AzQueue.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Services.Services;
+using System.Text;
+
+Services.Config.AppSettings.AzureStorageConnectionString = "your-connection-string";
+
+AzQueueService queue = new AzQueueService("sample-queue");
+
+//send
+//string base64message = Convert.ToBase64String(Encoding.UTF8.GetBytes("sample data"));
+//queue.SendMessageAsync(base64message).Wait(); 
+
+var message = queue.RetrieveNextMessageAsync().Result;
+
+//get
+//string text = Encoding.UTF8.GetString(Convert.FromBase64String(message.MessageText));
+//Console.WriteLine("Message is " + text);
+
+//delete
+await queue.DeleteMessage(message.MessageId, message.PopReceipt);
+
+Console.WriteLine("Message is deleted.");
---
+++ Services/Services/AzQueueService.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Services.Config;
+
+namespace Services.Services
+{
+    public class AzQueueService
+    {
+        private readonly QueueClient _queueClient;
+
+        public AzQueueService(string queueName)
+        {
+            _queueClient = new QueueClient(AppSettings.AzureStorageConnectionString, queueName);
+            _queueClient.CreateIfNotExists();
+        }
+
+        public async Task SendMessageAsync(string message) => await _queueClient.SendMessageAsync(message);
+
+        public async Task<QueueMessage> RetrieveNextMessageAsync()
+        {
+            QueueProperties properties = await _queueClient.GetPropertiesAsync();
+
+            if (properties.ApproximateMessagesCount > 0)
+            {
+                QueueMessage[] queueMessages = await _queueClient.ReceiveMessagesAsync(1, TimeSpan.FromMinutes(1));
+                
+                if (queueMessages.Any())
+                    return queueMessages[0];
+            }
+
+            return null;
+        }
+
+        public async Task DeleteMessage(string messageId, string popReceipt) => await _queueClient.DeleteMessageAsync(messageId, popReceipt);
+    }
+}
